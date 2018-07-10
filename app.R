ef5e4c45e53dc363cee4de888d8b859f9a4d2a7f
aswansyahputra <muhammadaswansyahputra@gmail.com>
2018-07-10 07:11:26 +0700

    Minor update

--- app.R
+++ app.R
@@ -50,6 +50,9 @@ server <- function(input, output, session) {
   # Konversi Tabulasi Data
   
   # Radar Plot
+  nilai_atributUtama() %>% 
+    mutate_if(is.numeric, scales::rescale) %>% 
+    ggradar(axis.label.size = 3, label.gridline.min = FALSE, label.gridline.mid = FALSE, label.gridline.max = FALSE, group.point.size = 3)
 
   
 }
